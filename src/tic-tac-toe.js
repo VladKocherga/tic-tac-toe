7135e46607d5436bf82c338759ec69eec36686e2
VladKocherga <vlad.kocherga228@gmail.com>
2021-07-15 22:03:12 +0300

    feat turns

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,123 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentPlayer = "x";
+    this.winner = null;
+    this.result = null;
+    this.turns = false;
+    this.matrix = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.matrix[rowIndex][columnIndex] === null) {
+      this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+      this.currentPlayer === "x"
+        ? (this.currentPlayer = "o")
+        : (this.currentPlayer = "x");
     }
+    this.matrixSearch();
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    if (this.winner === null) return false;
+    else return true;
+  }
 
+  getWinner() {
+    return this.winner;
+  }
+
+  noMoreTurns() {
+    return this.turns;
+  }
+
+  isDraw() {
+    if (this.result === "draw") {
+      return this.result;
+    } else {
+      return false;
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+  }
+  getFieldValue(rowIndex, colIndex) {
+    return this.matrix[rowIndex][colIndex];
+  }
+  matrixSearch() {
+    if (
+      this.matrix[0][0] === this.matrix[0][1] &&
+      this.matrix[0][1] === this.matrix[0][2]
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[1][0] === this.matrix[1][1] &&
+      this.matrix[1][1] === this.matrix[1][2]
+    ) {
+      this.winner = this.matrix[1][0];
+    } else if (
+      this.matrix[2][0] === this.matrix[2][1] &&
+      this.matrix[2][1] === this.matrix[2][2]
+    ) {
+      this.winner = this.matrix[2][0];
+      /*row*/
+    } else if (
+      this.matrix[0][0] === this.matrix[1][0] &&
+      this.matrix[2][0] === this.matrix[0][0]
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[0][1] === this.matrix[1][1] &&
+      this.matrix[2][1] === this.matrix[0][1]
+    ) {
+      this.winner = this.matrix[0][1];
+    } else if (
+      this.matrix[0][2] === this.matrix[1][2] &&
+      this.matrix[2][2] === this.matrix[0][2]
+    ) {
+      this.winner = this.matrix[0][2];
+    } else if (
+      this.matrix[0][0] === this.matrix[1][1] &&
+      this.matrix[1][1] === this.matrix[2][2]
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[2][0] === this.matrix[1][1] &&
+      this.matrix[2][2] === this.matrix[0][2]
+    ) {
+      this.winner = this.matrix[2][0];
     }
-
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  }
 }
+// let game;
 
+// game = new TicTacToe();
+// game.nextTurn(2, 1);
+// console.log(game.isFinished());
+// console.log(game.getWinner());
+// console.log(game.noMoreTurns());
+// console.log(game.isDraw());
+// console.log(game.getFieldValue(2, 1));
+// console.log(game.getCurrentPlayerSymbol());
+// game.nextTurn(2, 1);
+// console.log(game.isFinished());
+// console.log(game.getWinner());
+// console.log(game.noMoreTurns());
+// console.log(game.isDraw());
+// console.log(game.getFieldValue(2, 1));
+// console.log(game.getCurrentPlayerSymbol());
+// game.nextTurn(2, 2);
+// console.log(game.isFinished());
+// console.log(game.getWinner());
+// console.log(game.noMoreTurns());
+// console.log(game.isDraw());
+// console.log(game.getFieldValue(2, 2));
+// console.log(game.getCurrentPlayerSymbol());
+
+// console.log(game);
 module.exports = TicTacToe;
+console.log();
