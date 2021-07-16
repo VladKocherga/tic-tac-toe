6d10b248e5f980b126e893ec52ca1a3dcf9674ac
VladKocherga <vlad.kocherga228@gmail.com>
2021-07-16 13:08:11 +0300

    feat: end task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,9 +1,10 @@
 class TicTacToe {
   constructor() {
+    this.count = 0;
     this.currentPlayer = "x";
     this.winner = null;
     this.result = null;
-    this.turns = false;
+
     this.matrix = [
       [null, null, null],
       [null, null, null],
@@ -26,8 +27,10 @@ class TicTacToe {
   }
 
   isFinished() {
-    if (this.winner === null) return false;
-    else return true;
+    if (this.result === "draw" || this.winner != null) {
+      return true;
+    }
+    return false;
   }
 
   getWinner() {
@@ -35,15 +38,16 @@ class TicTacToe {
   }
 
   noMoreTurns() {
-    return this.turns;
+    for (let arr of this.matrix) {
+      if (arr.includes(null)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   isDraw() {
-    if (this.result === "draw") {
-      return this.result;
-    } else {
-      return false;
-    }
+    return this.result === "draw";
   }
   getFieldValue(rowIndex, colIndex) {
     return this.matrix[rowIndex][colIndex];
@@ -51,73 +55,69 @@ class TicTacToe {
   matrixSearch() {
     if (
       this.matrix[0][0] === this.matrix[0][1] &&
-      this.matrix[0][1] === this.matrix[0][2]
+      this.matrix[0][1] === this.matrix[0][2] &&
+      this.matrix[0][0] != null
     ) {
       this.winner = this.matrix[0][0];
     } else if (
       this.matrix[1][0] === this.matrix[1][1] &&
-      this.matrix[1][1] === this.matrix[1][2]
+      this.matrix[1][1] === this.matrix[1][2] &&
+      this.matrix[1][0] != null
     ) {
       this.winner = this.matrix[1][0];
     } else if (
       this.matrix[2][0] === this.matrix[2][1] &&
-      this.matrix[2][1] === this.matrix[2][2]
+      this.matrix[2][1] === this.matrix[2][2] &&
+      this.matrix[2][0] != null
     ) {
       this.winner = this.matrix[2][0];
       /*row*/
     } else if (
       this.matrix[0][0] === this.matrix[1][0] &&
-      this.matrix[2][0] === this.matrix[0][0]
+      this.matrix[2][0] === this.matrix[0][0] &&
+      this.matrix[0][0] != null
     ) {
       this.winner = this.matrix[0][0];
     } else if (
       this.matrix[0][1] === this.matrix[1][1] &&
-      this.matrix[2][1] === this.matrix[0][1]
+      this.matrix[2][1] === this.matrix[0][1] &&
+      this.matrix[0][1] != null
     ) {
       this.winner = this.matrix[0][1];
     } else if (
       this.matrix[0][2] === this.matrix[1][2] &&
-      this.matrix[2][2] === this.matrix[0][2]
+      this.matrix[2][2] === this.matrix[0][2] &&
+      this.matrix[0][2] != null
     ) {
       this.winner = this.matrix[0][2];
     } else if (
       this.matrix[0][0] === this.matrix[1][1] &&
-      this.matrix[1][1] === this.matrix[2][2]
+      this.matrix[1][1] === this.matrix[2][2] &&
+      this.matrix[0][0] != null
     ) {
       this.winner = this.matrix[0][0];
     } else if (
       this.matrix[2][0] === this.matrix[1][1] &&
-      this.matrix[2][2] === this.matrix[0][2]
+      this.matrix[1][1] === this.matrix[0][2] &&
+      this.matrix[2][0] != null
     ) {
       this.winner = this.matrix[2][0];
     }
+    if (this.winner === null) {
+      for (let i = 0; i < this.matrix.length; i++) {
+        if (this.matrix[i].includes(null)) {
+          this.count++;
+        }
+      }
+      if (this.count === 0) {
+        this.result = "draw";
+        this.turns = true;
+      } else {
+        this.count = 0;
+      }
+    }
   }
 }
-// let game;
 
-// game = new TicTacToe();
-// game.nextTurn(2, 1);
-// console.log(game.isFinished());
-// console.log(game.getWinner());
-// console.log(game.noMoreTurns());
-// console.log(game.isDraw());
-// console.log(game.getFieldValue(2, 1));
-// console.log(game.getCurrentPlayerSymbol());
-// game.nextTurn(2, 1);
-// console.log(game.isFinished());
-// console.log(game.getWinner());
-// console.log(game.noMoreTurns());
-// console.log(game.isDraw());
-// console.log(game.getFieldValue(2, 1));
-// console.log(game.getCurrentPlayerSymbol());
-// game.nextTurn(2, 2);
-// console.log(game.isFinished());
-// console.log(game.getWinner());
-// console.log(game.noMoreTurns());
-// console.log(game.isDraw());
-// console.log(game.getFieldValue(2, 2));
-// console.log(game.getCurrentPlayerSymbol());
-
-// console.log(game);
 module.exports = TicTacToe;
 console.log();
